852c199ccbe094f8e2d49e667126c6f45c57f061
塚田美幸 <tsukadamiyuki@tsukadamiyukinoMacBook-Air.local>
2026-07-23 23:26:37 +0900

    Initial commit

---
+++ himari.py
@@ -0,0 +1,56 @@
+import discord
+import os
+import json
+import random
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("HIMARI_TOKEN")
+
+HIMARI_CHANNEL_ID = 1529790625564590261
+
+with open("reply_list_himari.json", encoding="utf-8") as f:
+    REPLIES = json.load(f)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"{client.user} is awake!🐶")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.channel.id != HIMARI_CHANNEL_ID:
+        print(
+            "SKIPPED:",
+            repr(message.content),
+            "| CHANNEL:",
+            getattr(message.channel, "name", None),
+            "| CHANNEL ID:",
+            message.channel.id
+        )
+        return
+
+    print(
+        "RECEIVED:",
+        repr(message.content),
+        "| CHANNEL:",
+        getattr(message.channel, "name", None),
+        "| CHANNEL ID:",
+        message.channel.id
+    )
+
+    await message.channel.send(random.choice(REPLIES))
+
+client.run(TOKEN)
+
+
